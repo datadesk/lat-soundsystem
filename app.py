fa03b2c68bb15ca25b1042b071e6e911d941e0b1
palewire <ben.welsh@gmail.com>
2013-03-11 13:18:58 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import pygame
+from flask import Flask
+app = Flask(__name__)
+
+
+def play_sound(filename):
+    """
+    Play the provided sound file.
+    """
+    pygame.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
+
+@app.route("/rollout/")
+def rollout():
+    """
+    Play a snippet from Ludacris' "Rollout (My Business)"
+    """
+    play_sound("rollout.mp3")
+    return "ROOOOOLLLOUT!"
+
+
+if __name__ == "__main__":
+    app.run()
